fac9fdb42f03dcdd031ceec7b72f42f13a87922c
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-05-19 09:07:19 -0400

    tox

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "deflake",
@@ -17,6 +11,5 @@ setup(
     license = "Public Domain",
     keywords = "testing flakey",
     url = "http://packages.python.org/deflake",
-    long_description=read('README.md'),
     scripts = ["deflake.py"]
 )
