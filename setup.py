5a13e139406ffcf4f9f9c579b3fb4cc228197cfc
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-05-22 10:07:20 -0400

    Got running as script

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import setup
 
 setup(
     name = "deflake",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "NCBI",
     author_email = "",
     description = ("A program to debug flakey programs"),
     license = "Public Domain",
     keywords = "testing flaky",
     url = "http://packages.python.org/deflake",
-    scripts = ["deflake/deflake.py"],
+    entry_points = {
+        'console_scripts': 
+            ['deflake=deflake.cli:main']
+    },
     packages = ["deflake"]
 )
