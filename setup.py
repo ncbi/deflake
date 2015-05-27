a01bea1f9ab22b826caf6bf8e7f9a277f9ee370d
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-05-27 10:57:16 -0400

    Added better description

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 setup(
     name = "deflake",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "NCBI",
     author_email = "",
-    description = ("A program to debug flakey programs"),
+    description = ("Helps debug a non determinate test (or any flaky program) by running it until it exits with a non-zero exit code. See https://github.com/ncbi/deflake for more details"),
     license = "Public Domain",
     keywords = "testing flaky",
     url = "http://packages.python.org/deflake",
