2d89db2498d4bbc349b54caa241afb86e5fce658
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-06-04 11:03:19 -0400

    Be sure about com

--- deflake/deflake.py
+++ deflake/deflake.py
@@ -89,7 +89,12 @@ class Deflake(object):
                 self._output("FAIL (run %s)" % (str(self._loops * self.pool_size + i + 1)), 
                     self._quiet,
                     process_passed=False)
-                self._output(com[1], self._quiet, process_passed=False)
+
+                # Print error message if we have stderr
+                try:
+                    self._output(com[1], self._quiet, process_passed=False)
+                except TypeError, IndexError: 
+                    pass
                 self._process_failed = True
                 break
 
