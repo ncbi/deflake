739e3a1c7d838b13a43297464c31c76c6877eb86
hellmanj <hellmanj@ncbi.nlm.nih.gov>
2015-06-18 15:51:17 -0400

    refactor to use standard library for multiprocessing

--- deflake/deflake.py
+++ deflake/deflake.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import signal
 
 """
 Classes and script for debugging a flaky program.
@@ -7,6 +8,7 @@ Type python deflake.py -h on the command-line to see usage.
 """
 
 import subprocess
+from multiprocessing import Pool
 
 
 class _Printer(object):
@@ -22,6 +24,41 @@ class _Printer(object):
         print(out)
 
 
+def init_worker():
+    """
+    Makes worker processes ignore ctrl+c so Deflake.run can catch it
+    see http://stackoverflow.com/a/1408476/725604
+    """
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+
+def run_cmd(command, procnum, counter_token):
+    """
+    Worker process which executes the command to be deflaked
+    :param command: command to be deflaked
+    :type command: str
+    :param procnum: process number to be used as a replacement for the counter_token
+    :type procnum: int
+    :param counter_token: counter_token to be replaced by the process number
+    :type counter_token: str
+    :return: PASS or FAIL message (FAIL message includes stderr)
+    """
+    cmd = command.replace(counter_token, str(procnum))
+    p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    com = p.communicate()
+    result = p.returncode
+    if result == 0:
+        return "PASS"
+    else:
+        res = "FAIL (run %s)\n" % procnum
+        # Print error message if we have stderr
+        try:
+            res += com[1]
+        except (TypeError, IndexError):
+            pass
+        return res
+
+
 class Deflake(object):
     _printer = _Printer()
     _process_failed = False
@@ -31,7 +68,7 @@ class Deflake(object):
     def __init__(self, command, max_runs=10, pool_size=1, counter_token='#count#', quiet=False):
         """
         :param command: The command to run
-        :type param: str
+        :type command: str
         :param max_runs: The maximum runs to execute if command doesn't return non-zero exit status
         :type max_runs: int
         :param pool_size: The number of processes in each batch to multiprocess until max_runs is reached
@@ -49,68 +86,46 @@ class Deflake(object):
         self._processes = []
         self._quiet = quiet
 
+        self.pool = Pool(processes=self.pool_size, initializer=init_worker)
+
         # Collect output as data
         # so we can return from run()
         self._out = []
 
-    def _output(self, str, quiet, process_passed=True):
+    def _output(self, outstr, quiet, process_passed=True):
         """ Wrapper for _Printer.out(). Outputs
         to stdout with proper color, and saves
         output to data structure in order to return
         data from run()"""
         if not quiet:
             color = "OKGREEN" if process_passed else "FAIL"
-            self._printer.out(str, color)
-        self._out.append(str)
+            self._printer.out(outstr, color)
+        self._out.append(outstr)
 
-    def _get_processes(self):
-        ret = []
-        last_batch = self.max_runs % self.pool_size
-        range_stop = last_batch if len(self._processes) >= self.max_runs/self.pool_size else self.pool_size
-        for i in range(range_stop):
-            cmd = self.command.replace(self.counter_token, str(self._processes_run + i + 1))
-            p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            ret.append(p)
-        self._processes_run += len(ret)
-
-        # Keep track of the process pools so far since
-        # this method is called by its parent method
-        # recursively.
-        self._processes.append(ret)
-        return ret
-
-    def _run_processes(self):
-        for i, p in enumerate(self._get_processes()):
-            com = p.communicate()
-            result = p.returncode
-            if result == 0:
-                self._output("PASS", self._quiet)
-            else:
-
-                self._output("FAIL (run %s)" % (str(self._loops * self.pool_size + i + 1)), 
-                    self._quiet,
-                    process_passed=False)
-
-                # Print error message if we have stderr
-                try:
-                    self._output(com[1], self._quiet, process_passed=False)
-                except TypeError, IndexError: 
-                    pass
-                self._process_failed = True
-                break
-
-        self._loops = self._loops + 1
-        if not self._process_failed and self._processes_run < self.max_runs:
-            self._run_processes()
-        return self._out
+    def check_result(self, result):
+        if result == 'PASS':
+            self._output(result, self._quiet)
+        else:
+            self._output(result, self._quiet, process_passed=False)
+            self.pool.terminate()
 
     def run(self):
         """ Runs the deflaking. 
         Method prints results to stdout and
         returns a list of output.
         """
-        results = self._run_processes()
-
-        # Take out trailing newlines from result list if necessary.
-        ret = results[:-1] if results[-1] == "\n" else results 
-        return ret 
+        try:
+            for i in range(self.max_runs):
+                try:
+                    self.pool.apply_async(run_cmd,
+                                          args=(self.command, i + 1, self.counter_token),
+                                          callback=self.check_result).get(9999999)
+                except AssertionError:
+                    break
+            self.pool.close()
+        except KeyboardInterrupt:
+            print("\nYou cancelled the job!")
+            self.pool.terminate()
+        finally:
+            self.pool.join()
+        return self._out
